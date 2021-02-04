0f8eacf4b205fbe166e017cf241896c691227312
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-02-05 01:00:09 +0530

    Update Add_Contact.java
    Ability to edit
    existing contact
    person using their
    name

--- Add_Contact.java
+++ Add_Contact.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class AddressBookMain {
     public String first_Name, last_Name, address, city, state, email_Id;
     public String zip_Code, phone_Number;
-
-
     public AddressBookMain(String first_Name, String last_Name, String address, String city, String state, String zip_Code, String phone_Number, String email_Id) {
         this.first_Name = first_Name;
         this.last_Name = last_Name;
@@ -16,6 +14,7 @@ public class AddressBookMain {
         this.email_Id = email_Id;
 
     }
+
     public void displayContacts() {
 
         System.out.println("First Name: " + first_Name);
@@ -27,18 +26,22 @@ public class AddressBookMain {
         System.out.println("Phone Number: " + phone_Number);
         System.out.println("Email Id: " + email_Id);
     }
-    public static void main(String[] args) {
-        int i = 0,choice,max_Num_Contacts;
+
+
+
+    public static void main(String[] args) throws NullPointerException {
+        int i = 0,choice,max_Num_Contacts,value;
 
         String first_Name, last_Name, address, city, state, email_Id;
         String zip_Code, phone_Number;
 
         Scanner scanner = new Scanner(System.in);
+        AddressBookSubClass addressBookSubClass = new AddressBookSubClass();
 
         System.out.println("ENTER THE MAXIMUM NUMBER OF CONTACTS");
         max_Num_Contacts = scanner.nextInt();
-        AddressBookMain[] addressBookMainArray = new AddressBookMain[max_Num_Contacts];
 
+        AddressBookMain[] addressBookMainArray = new AddressBookMain[max_Num_Contacts];
         do {
             System.out.println("Enter the contact details");
             System.out.println("                         ");
@@ -65,18 +68,35 @@ public class AddressBookMain {
 
             System.out.println("enter the email id");
             email_Id = scanner.next();
-
             addressBookMainArray[i] = new AddressBookMain(first_Name, last_Name, address, city, state, zip_Code, phone_Number, email_Id);
             System.out.println("ENTER 0 FOR ADDING CONTACT AND 1 TO EXIT");
             choice = scanner.nextInt();
             i++;
-        } while( choice != 1 && i < max_Num_Contacts);
+        } while (choice != 1 && i < max_Num_Contacts);
+        System.out.println("ENTER THE CHOICE \n 1: TO DISPLAY CONTACT DETAILS\n 2: TO EDIT CONTACT DETAILS\n 3:TO EXIT");
+        value = scanner.nextInt();
 
-        for (int j = 0; j < i; j++) {
-            System.out.println("                                 ");
-            System.out.println("CONTACT " + (j+1) + " DETAILS ARE:");
-            addressBookMainArray[j].displayContacts();
+        switch (value) {
+            case 1:
+                for (int j = 0; j < i; j++) {
+                    System.out.println("                                 ");
+                    System.out.println("CONTACT " + (j + 1) + " DETAILS ARE:");
+                    addressBookMainArray[j].displayContacts();
+                }
+                break;
+            case 2:
+                if(i>0) {
+
+                    System.out.println("ENTER THE NAME OF CONTACT TO EDIT");
+                    String first_name = scanner.next();
+                    addressBookSubClass.editContactDetails(first_name,addressBookMainArray);
+                    break;
+                } else {
+                    System.out.println("NO CONTACTS AVAILABLE TO EDIT");
+                    break;
+                }
+            case 3:
+                break;
         }
-
     }
 }
