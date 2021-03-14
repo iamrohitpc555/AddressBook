da297dfdceb1fe31027e475a2ffb1255439a053f
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-03-14 16:54:20 +0530

    Ability for the AddressBook Service to rerieve all the Entries from the DB

---
+++ src/test/java/org/example/AddressBookServiceTest.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AddressBookServiceTest {
+
+    @Test
+    public void test() {
+        try {
+            Connection con = JDBCConnection.getInstance().getConnection();
+            Statement stmt = con.createStatement();
+            String query = "select count(contact_id) from contact";
+            ResultSet resultSet = stmt.executeQuery(query);
+            resultSet.next();
+            int expectedOutput = resultSet.getInt("count(contact_id)");
+            CrudOperations crudOperations = new CrudOperations();
+            crudOperations.readAll();
+            int output = AddressBookMain.addressBookMap.get("AB1").contactList.size();
+            Assert.assertEquals(expectedOutput, output);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
